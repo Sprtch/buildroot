d8172d6b4c519c46e62c042687f8cf9b2728383f
tperale <perale.thomas@gmail.com>
2020-08-31 00:40:46 +0200

    [enh] Switch huron package to automated installation with python-package

--- package/huron/huron.mk
+++ package/huron/huron.mk
@@ -4,25 +4,23 @@
 #
 ################################################################################
 
-HURON_VERSION = 0.0.1
+HURON_VERSION = 5258bc49fe38bc984354964b283a516659f07ba9
 HURON_SITE = $(call github,Sprtch,huron,$(HURON_VERSION))
 HURON_LICENSE = MIT
 HURON_LICENSE_FILES = LICENSE
+HURON_SETUP_TYPE = setuptools
 
-define HURON_BUILD_CMDS
+
+define HURON_BUILD_FRONTEND
 	$(TARGET_MAKE_ENV) npm install --prefix $(@D)
 	$(TARGET_MAKE_ENV) npm run build --prefix $(@D)
 endef
 
-define HURON_INSTALL_TARGET_CMDS
+HURON_PRE_BUILD_HOOKS += HURON_BUILD_FRONTEND
+
+define HURON_POST_INSTALL_HOOKS
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_sprtch_PATH)/package/huron/nginx.conf \
 		$(TARGET_DIR)/etc/nginx/nginx.conf
-	$(INSTALL) -d $(TARGET_DIR)/www/huron
-	$(INSTALL) -D -m 0644 $(@D)/requirements.txt $(TARGET_DIR)/www/huron/requirements.txt
-	$(INSTALL) -D -m 0644 $(@D)/README.md $(TARGET_DIR)/www/huron/README.md
-	$(INSTALL) -D -m 0644 $(@D)/setup.py $(TARGET_DIR)/www/huron/setup.py
-	$(INSTALL) -D -m 0755 $(@D)/manage.py $(TARGET_DIR)/www/huron/manage.py
-	cp -r $(@D)/build $(@D)/huron $(TARGET_DIR)/www/huron/
 endef
 
 define HURON_INSTALL_INIT_SYSV
@@ -30,4 +28,4 @@ define HURON_INSTALL_INIT_SYSV
 		$(TARGET_DIR)/etc/init.d/S99gunicorn
 endef
 
-$(eval $(generic-package))
+$(eval $(python-package))
