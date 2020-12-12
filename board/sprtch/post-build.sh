c16ba8aab2d739dc3c8d3e59fb271eb80ff55158
Thomas Perale <perale.thomas@gmail.com>
2020-12-12 16:45:04 +0100

    wpa_supplicant: add the password with pass

--- board/sprtch/post-build.sh
+++ board/sprtch/post-build.sh
@@ -5,7 +5,10 @@ set -e
 
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
+	grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab ||
+		sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+wpa_passphrase "$(pass sprtch_home_ssid)" "$(pass sprtch_home_pass)" >>"${TARGET_DIR}/etc/wpa_supplicant.conf"
+wpa_passphrase "$(pass sprtch_sprtch_ssid)" "$(pass sprtch_sprtch_pass)" >>"${TARGET_DIR}/etc/wpa_supplicant.conf"
